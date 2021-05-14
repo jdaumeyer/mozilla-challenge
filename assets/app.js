74f87a2839a3a8561a5b2cc714411b99edb76ade
Jackson Daumeyer <jed.chancey@gmail.com>
2021-05-14 16:05:14 -0400

    Finished style of detail page, fixed header pad

--- assets/app.js
+++ assets/app.js
@@ -4,32 +4,28 @@
 	// Add Scripts Here
 
 	// Top Nav
-	const toggleTopNavButton = document.querySelector(".topNavToggle")
+	const topNavButton = document.querySelector(".topNavToggle")
 	const topNav = document.querySelector(".topNav")
 
 	// Show Details
 	const showDetailsButton = document.getElementById("show-details")
-	const details = document.getElementsByClassName("email-details")[0]
-
-
-	var toggleTopNav = function() {
-		topNav.getElementsByTagName("ul")[0].classList.toggle("invisible")
-
-		toggleTopNavButton.getElementsByTagName("i")[0].classList.toggle("flip-forward");
-	}
+	const details = document.getElementsByClassName("info")
 
 	/*
 	 * toggles visiblilty of a subtree and animates the button
 	 *
 	 */
 	var toggleMenu = function(element, button) {
-		element.classList.toggle("invisible")
+		for (let e of element) {
+			e.classList.toggle("closed")
+			console.log(e)
+		}
 		button.getElementsByTagName("i")[0].classList.toggle("flip-forward");
 
 	}
 
-	toggleTopNavButton.addEventListener("click", function(e){
-		toggleMenu(topNav.getElementsByTagName("ul")[0], toggleTopNavButton)
+	topNavButton.addEventListener("click", function(e){
+		toggleMenu(topNav.getElementsByTagName("ul"), topNavButton)
 	})
 
 	showDetailsButton.addEventListener("click", function(e) {
