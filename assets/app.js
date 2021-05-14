264cf3f803a43eb49dbe22ddf34791e47a3e0024
Jackson Daumeyer <jed.chancey@gmail.com>
2021-05-14 09:06:01 -0400

    Header is functionally done

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,14 @@
 (function() {
 	"use strict";
 
+	const toggleTopNavButton = document.querySelector(".topNavToggle")
+	const topNav = document.querySelector(".topNav")
 	// Add Scripts Here
-    	
+	var toggleTopNav = function() {
+		topNav.getElementsByTagName("ul")[0].classList.toggle("invisible")
+	}
+
+	toggleTopNavButton.addEventListener("click", function(e){
+		toggleTopNav()
+	})
 })();
