09731e3189411ac57b1a8fc600ac28154c641f12
Jackson Daumeyer <jed.chancey@gmail.com>
2021-05-17 12:39:50 -0400

    Added semicolons and improved readability

--- assets/app.js
+++ assets/app.js
@@ -4,62 +4,76 @@
 	// Add Scripts Here
 
 	// Top Nav
-	const topNavButton = document.querySelector(".topNavToggle")
-	const topNavList = document.querySelector(".topNav").getElementsByTagName("ul")
+	const topNavButton = document.querySelector(".topNavToggle");
+	const topNavList = document.querySelector(".topNav").getElementsByTagName("ul");
 
 	// Show Details
-	const showDetailsButton = document.getElementById("show-details")
-	const details = document.getElementsByClassName("info")
+	const showDetailsButton = document.getElementById("show-details");
+	const details = document.getElementsByClassName("info");
 
 	// Copy Button
-	const copyButton = document.getElementsByClassName("copy")[0]
-	const copyMessage = document.getElementById("copy-message")
+	const copyButton = document.getElementsByClassName("copy")[0];
+	const copyMessage = document.getElementById("copy-message");
 
 
+
+	/*
+	 * ======================
+	 * = Desktop Breakpoint =
+	 * ======================
+	 */
 	if (window.screen.width >= 600) {
 
 		// Disable the menu button on large screens
-		topNavButton.classList.toggle("invisible")
-		topNavList[0].classList.toggle("closed")
+		topNavButton.classList.toggle("invisible");
+		topNavList[0].classList.toggle("closed");
 
 		// Show hidden elements on large screens
-		// These are in loops because I assume there would be multiple on the actual site
+		// These are in loops because I assume there would be multiple on an actual site
 		for (let e of document.getElementsByClassName("forwardto")) {
-			e.classList.toggle("invisible")
+			e.classList.toggle("invisible");
 		}
 		for (let e of document.getElementsByClassName("change-name")) {
-			e.classList.toggle("invisible")
+			e.classList.toggle("invisible");
 		}
 	}
 
 	/*
-	 * toggles visiblilty of a subtree and animates the button
-	 *
+	 * Toggles a list of elements and animates the associated button (if needed)
 	 */
-	var toggleMenu = function(element, button) {
-		for (let e of element) {
-			e.classList.toggle("closed")
+	var toggleElements = function(elements, button=null) {
+		for (let e of elements) {
+			e.classList.toggle("closed");
 		}
-		button.getElementsByTagName("i")[0].classList.toggle("flip-forward");
 
+		button?.getElementsByTagName("i")[0].classList.toggle("flip-forward");
 	}
 
+
+	/*
+	 * ===================
+	 * = Event Listeners =
+	 * ===================
+	 */
+
 	topNavButton.addEventListener("click", function(e){
-		toggleMenu(topNavList, topNavButton)
+		toggleElements(topNavList, topNavButton);
 	})
 
 	showDetailsButton.addEventListener("click", function(e) {
-		toggleMenu(details, showDetailsButton)
+		toggleElements(details, showDetailsButton);
 	})
 
 	copyButton.addEventListener("click", function(e) {
-		copyMessage.classList.toggle("closed")
-		copyMessage.style.top = ((e.clientY - 50) + document.documentElement.scrollTop) + "px"
-		copyMessage.style.left = (e.clientX - 40) + "px"
+		// Display Copy message and set it's position
+		copyMessage.classList.toggle("closed");
+		copyMessage.style.top = ((e.clientY - 50) + document.documentElement.scrollTop) + "px";
+		copyMessage.style.left = (e.clientX - 40) + "px";
 
+		// Hide copy message again after a bit
 		setTimeout(function() {
-			copyMessage.classList.toggle("closed")
-		}, 1000)
+			copyMessage.classList.toggle("closed");
+		}, 1500);
 	})
 
 })();
